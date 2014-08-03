36eac0da7d28a38054b3559feae2f1cbc4af725d
Bryan Mulvihill <bmulvihill@pinsonault.com>
2014-08-03 11:28:47 -0400

    fix user bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,7 @@ class UsersController < ApplicationController
 	def show
 	  @user = User.find(params[:id].to_s)
 	end
+
+  def edit
+  end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 #Deploy code to production server
-role :app, %w{ec2-user@54.191.168.243}
-role :web, %w{ec2-user@54.191.168.243}
-role :db,  %w{ec2-user@54.191.168.243}
+role :app, %w{ec2-user@54.186.30.96}
+role :web, %w{ec2-user@54.186.30.96}
+role :db,  %w{ec2-user@54.186.30.96}
 
 set :ec2_access_key_id, nil
 set :ec2_secret_access_key, nil
